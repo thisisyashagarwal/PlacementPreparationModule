000e8ae3051c8cf3c803d0e5f83d13d1a2cd6b6b
Yash Agarwal <59206738+thisisyashagarwal@users.noreply.github.com>
2022-08-04 16:14:52 +0530

    Update NextPermutation.cpp

--- Week1/NextPermutation.cpp
+++ Week1/NextPermutation.cpp
@@ -3,24 +3,29 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
         int idx1, idx2;
-        for( int i=0; i<n; i++)
+        for( int i=n-2; i>=0; i--)
         {
-            if( nums[i] > nums[i+1])
+            if( nums[i] < nums[i+1])
             {
                 idx1 = i;
                 break;
             }
         } 
-        for( int i=n-1; i>=n; i--)
+        if( idx1 < 0)
+            reverse( nums.begin(), nums.end());
+        else
         {
-            if( nums[i] < nums[i-1])
+            for( int i=n-1; i>=0; i--)
             {
-                idx2 = i-1;
-                break;
+                if( nums[i] > nums[idx1])
+                {
+                    idx2 = i;
+                    break;
+                }
             }
+            swap( nums[idx1], nums[idx2]);
+            int k = 0;
+            reverse( nums.begin() + idx1  +1, nums.end());
         }
-        swap( nums[idx1], nums[idx2]);
-        reverse( nums.begin(), nums.end());
-        
     }
 };
